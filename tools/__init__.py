e63b5c9ebfbed51b6cd3a0e8c7fb10473b6fd774
Dron939-sketch <Dron939@yandex.ru>
2026-04-07 09:18:30 +0300

    Create __init__.py

---
+++ tools/__init__.py
@@ -0,0 +1,10 @@
+# tools/__init__.py
+"""
+Инструменты для AI-агента
+"""
+
+from .search_tool import SearchTool
+from .reminder_tool import ReminderTool
+from .code_tool import CodeTool
+
+__all__ = ['SearchTool', 'ReminderTool', 'CodeTool']
